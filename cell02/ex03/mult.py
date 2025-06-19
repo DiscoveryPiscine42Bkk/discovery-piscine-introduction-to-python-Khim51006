4f619bbaef62862dcff6d57b9f951b7777682581
Khim51006 <phannitakhim@gmail.com>
2025-06-19 10:07:36 +0000

    add 42

---
+++ cell02/ex03/mult.py
@@ -0,0 +1,6 @@
+a = int(input())
+b = int(input())
+r = a * b
+print(f"{a} x {b} = {r}")
+print("The result is positive." if r > 0 else "The result is negative." if r < 0 ealse "The result is positive and negative.")
+
